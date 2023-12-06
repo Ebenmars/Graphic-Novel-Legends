45858b58233052eefc29657857da4c2d109444aa
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-12-05 19:03:23 -0500

    Inital

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>Graphic Novel Legends</h1>
+        <p>
+          
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
