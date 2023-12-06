347cd2ab7c4070c86ac1a1212030f75529a5e2af
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-12-05 22:58:11 -0500

    Added Pictures And Made Heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+function Header() {
+  return ( <header>
+    <img src="src/assets/Neil.png" alt="Neil Gaiman" />
+    <img src="src/assets/brian.png" alt="Brian K. Vaughan" />
+    <img src="src/assets/alan.png" alt="Alan Moore" />
+    <img src="src/assets/frank.png" alt="Frank Miller" />
+    <h1>Graphic Novel Legends</h1>
+    <p>
+      Legends of the graphi novel and comic world
+    </p>
+  </header>
+  );
+}
+
+
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>Graphic Novel Legends</h1>
-        <p>
-          
-        </p>
-      </header>
+      <Header></Header>
       <main>
         <h2>Time to get started!</h2>
       </main>
