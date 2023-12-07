146da7e49d0d61136cd05ee557260129230808e3
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-12-06 21:49:28 -0500

    Put Every Section Of The Code In It Own File

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
 
 import { BIOS } from "./data.js";
-
-const subtext = ["Legends", "Icons", "Innovators", "Revolutionaries"];
-
-function getRandomWord() {
-  return Math.floor(Math.random() * subtext.length);
-}
-
-const words = subtext[getRandomWord()];
-
-function Header() {
-
-  return (<header>
-    <img src={BIOS[0].image} alt="Alan Moore" />
-    <img src={BIOS[1].image} alt="Neil Gaiman" />
-    <img src={BIOS[2].image} alt="Brian K. Vaughan" />
-    <img src={BIOS[3].image} alt="Frank Miller" />
-    <h1>Graphic Novel Legends</h1>
-    <p>
-      {words} Of The Genre
-    </p>
-  </header>
-  );
-}
-
-function Bios(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header.jsx";
+import Bios from "./components/Bios.jsx";
 
 function App() {
   return (
@@ -40,7 +9,7 @@ function App() {
       <Header></Header>
       <main>
         <section id="bios">
-          <h2>{words}</h2>
+          <h2>{}</h2>
           <ul>
             <Bios title={BIOS[0].title}
               description={BIOS[0].description}
